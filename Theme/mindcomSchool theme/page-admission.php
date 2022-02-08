c7cd45c8ddaea94e161ee5a8c6ca6b9d5a506fff
lucky90354 <luckychauhan7478844@gmail.com>
2022-02-08 14:15:37 +0530

    initial commit

---
+++ Theme/mindcomSchool theme/page-admission.php
@@ -0,0 +1,77 @@
+<?php get_header() ?>
+<!--
+ Template Name: Admission
+-->
+<!--================================================== 
+            Global Page Section Start
+    ================================================== -->
+<section class="global-page-header" style="background-image:url('/wp-content/uploads/2021/10/banner-1.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="block">
+                    <h2><?php the_title(); ?></h2>
+                    <ol class="breadcrumb">
+                        <li>
+                            <a href="/">
+                                <i class="ion-ios-home"></i>
+                                Home
+                            </a>
+                        </li>
+                        <li class="active"><?php the_title(); ?></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+    <section class="page-header" style="background-image:url('/wp-content/uploads/2021/11/seetapur.jpg'); top:0;">
+    <form action="http://shhss.clone.mindcms.tech">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="block" style="text-align: center; top:50px;">
+                        <h2>NURSERY ADMISSION FOR ACADEMIC SESSION 2022-2023</h2><br>
+                        <div class="importantpoints" style="text-align: left; background-color:transparent;">
+                            <h4>Online Application Forms will be available in the School Website www.sacredheartsitapur.org from 02/12/2021 to 04/12/2021. Application Fee Rs. 500/-</h4>
+                            <ul class="breadcrumb" style="text-align: left; background-color:transparent;">
+                                <h5>PLEASE NOTE :-</h5>
+                                <br>1-Admission is open to Nursery Class only.
+                                <br>2- The minimum age of the candidate should be 3 years on 1st April 2022. However, it should not exceed 4 years on 1st April 2022.
+                                <br>3- The particulars of the Child (i.e. Name, Parent’s Name, and Date of Birth) should be filled in correctly. The date of the birth filled in the form must tally with the one recorded in the Date of Birth Certificate issued by the Municipal Authority as well as the Maternity Hospital.
+                                <br>4- Incomplete form will not be processed.
+                                <br>5- At the time of submission of Application form it is mandatory to attach all the necessary documents attested by the Gazetted Officer. The necessary documents are mentioned below.
+                                <br>&nbsp;&nbsp;• Birth Certificate from the Municipal Corporation / Nagar Nigam.
+                                <br>&nbsp;&nbsp;• A recent Photograph of the candidate with parents.
+                                <br>&nbsp;&nbsp;• Parent’s school document, If Alumni of St. Francis’ College, Lucknow (Class X or XII).
+                                <br>&nbsp;&nbsp;• Sibling’s ( not cousins ) school document, if studying in this college.
+
+                                <br>6- Parents are advised to apply to other schools as well, due to limited seats (160) at Sacred Heart Hr. Sec. School
+                                <br>7- Having a sibling enrolled as a student Sacred Heart Hr. Sec. School does not guarantee admission of a candidate in Nursery.
+                                <br>8- The Management of the School reserves all rights of Admission or Rejection and is not bound to give Reasons for the same.
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="down-heading" style="text-align: center;">
+                        <h4>APPLICATION IS NOT A GUARANTEE FOR ADMISSION.</h4>
+                        <h4> APPLICATION FORM FEE IS NOT REFUNDABLE.</h4>
+                    </div>
+                    <div class="agreebuttton" style="text-align: center;">
+                        <button id="iagree" type="submit" style="padding:5px; justify-content:space-between; color:green; background-color:transparent;">I Accept</button>
+                        <button id="idonotagree" style="padding:5px; justify-content:space-between; color:red; background-color:transparent">I Do Not Accept</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </form>
+    </section>
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<?php get_footer(); ?>
